ff0670eba38b10ca973726a9413f484589567659
daria <deshkadary2@gmail.com>
2022-09-29 09:56:26 +0700

    Add project files.

---
+++ points/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace points
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(275, 14);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(512, 422);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private Button button1;
+    }
+}
---
+++ points/Points.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Разработать классы Point (точка на плоскости) и Points
+//(множество точек на плоскости). Перегрузить операции + (точка) —
+//добавляет точку в множество, - (точка) — убирает точку из множества,
+//если она в нем присутствует, + (множество точек) — объединение
+//множеств, - (множество точек) — вычитание множеств, == - для точки и
+//для множества, != - для точки и для множества.
+//добавить обработчики исключений (не менее трёх
+//исключительных ситуаций).
+
+namespace points
+{
+    class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public static bool operator ==(Point left, Point right)
+        {
+            if (left.x == right.x && left.y == right.y) return true;
+            return false;
+        }
+
+        public static bool operator !=(Point left, Point right)
+        {
+            if(left == right) return false;
+            return true;
+        }
+    }
+    class Points
+    {
+        public List<Point> points = new();
+
+		public static Points operator +(Points left, Point right)
+		{
+            left.points.Add(right);
+            return left;
+		}
+
+        public static Points operator -(Points left, Point right)
+        {
+            if(left.points.Contains(right)) left.points.Remove(right);
+            return left;
+        }
+
+        public static Points operator +(Points left, Points right)
+        {
+            left.points.AddRange(right.points);
+            return left;
+        }
+
+        public static Points operator -(Points left, Points right)
+        {
+            //Points result = new Points();
+            //result.points.AddRange(left.points);
+            //result.points.AddRange(right.points);
+            //return result;
+            foreach (var point in right.points)
+            {
+                if (left.points.Contains(point)) left.points.Remove(point);
+            };
+            return left;
+        }
+
+        public static bool operator ==(Points left, Points right)
+        {
+            if (left == right && left == right) return true;
+            return false;
+        }
+
+        public static bool operator !=(Points left, Points right)
+        {
+            if (left == right) return false;
+            return true;
+        }
+    }
+}
